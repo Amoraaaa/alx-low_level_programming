6b9bf79ce4bc0f29e31b277b382c45fe2f649b09
Amoraaaa <ammarking503@gmail.com>
2023-07-13 20:00:38 -0700

    n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stadio.h>
-
 /**
  * main - Entry point
  *
  * Description: A C program that prints with put function
  *
- * Return: always 0 (success)
- */
+ * Return: Always 0 (Success)
+*/
 
 int main (void)
 {
