cb4dd6e987f15bfde48d1123eb33d098ff1e9b5b
Amoraaaa <ammarking503@gmail.com>
2023-07-21 12:16:52 -0700

    mfj

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * program that prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	for (int i = 1; i <= 100; i++)
+	fizzbuzz(100);
+	return (0);
+}
+
+void fizzbuzz(int n)
+{
+	for (int i = 1; i <= n; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz ");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
-			printf("\n");
-			return (0);
+	printf("\n");
 }
 
