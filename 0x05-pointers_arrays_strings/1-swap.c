bfac0d27e34c103fba6ea4ca4beead89c5c759be
Amoraaaa <ammarking503@gmail.com>
2023-07-24 19:31:30 -0700

    P

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,15 +2,14 @@
 
 /**
  * reset_to_98 - updates the value it points to to 98
+ *
  * @n: a pointer to an int
- */
+ *
+ * Return: Nothing
+*/
+
 void reset_to_98(int *n)
 {
-	/* check if n is not null */
-	if (n != NULL)
-	{
-		/* dereference n and assign 98 to it */
-		*n = 98;
-	}
+	*n = 98;
 }
 
