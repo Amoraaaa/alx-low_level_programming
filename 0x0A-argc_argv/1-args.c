3073d56332133782dfefa700804d7b1a33ce7c78
Amoraaaa <ammarking503@gmail.com>
2023-08-05 12:10:31 -0700

    d

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,17 +2,16 @@
 
 /**
  * main - prints the number of arguments passed into it
- * @argc: number of arguments
- * @argv: array of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
  * Return: 0 on success
-*/
-
-int main(int argc, char *argv[])
+ */
+int main(int argc, char **argv)
 {
-	if (argc >= 0) /* check if argument is valid */
-	{
-		printf("%d\n", argc - 1); /* print the number of arguments */
-	}
-	return (0); /* return 0 on success */
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
 }
+
 
