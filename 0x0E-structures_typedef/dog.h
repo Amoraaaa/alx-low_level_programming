cd66e6d3b1d5c4e7dbe10d4578d739f484bb713b
Amoraaaa <ammarking503@gmail.com>
2023-08-15 17:38:32 -0700

    s

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,6 +10,8 @@
  * @owner: owner of dog
  *
  * Description: Longer description
+ *
+ * Return: return (0)
  */
 typedef struct dog
 {
@@ -17,4 +19,4 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
