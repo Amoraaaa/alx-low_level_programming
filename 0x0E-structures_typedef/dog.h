baf4024f499c4bb60b61f51d12126e5a575cbe31
Amoraaaa <ammarking503@gmail.com>
2023-08-15 17:32:40 -0700

    s

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - a new type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
