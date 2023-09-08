771cceb0af6077b93fe82d4974c8608b6ab48b38
Amoraaaa <ammarking503@gmail.com>
2023-09-08 10:08:09 -0700

    fh

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, n_written;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		n_written = write(fd, text_content, _strlen(text_content));
+		if (n_written == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+
+	return (i);
+}
+
